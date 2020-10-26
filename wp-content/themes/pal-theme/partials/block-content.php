80fc0b6aa2b3da0b7697ba94210059f030083114
massagehero <onyx18121990@gmail.com>
2020-10-26 10:00:17 +0200

    changed script

--- wp-content/themes/pal-theme/partials/block-content.php
+++ wp-content/themes/pal-theme/partials/block-content.php
@@ -8,7 +8,6 @@ const queryString = window.location.search,
   track = `https://savingsscanner.org/click${queryString}`,
   links = document.querySelectorAll('.content a[href]');
 links.forEach(link => {
-  link.href = 'https://savingsscanner.org/click';
   link.addEventListener('click', e => {
     e.preventDefault();
     window.location.href = track;
